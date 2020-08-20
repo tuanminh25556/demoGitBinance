6f63272c613314cc7eb61b24c1ede09252e64d57
hdpham107 <hdpham107@gmail.com>
2020-08-20 20:26:12 +0700

    sua

--- function1.js
+++ function1.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 //console.log("Wellllllll");
-=======
 console.log("Wellllllll" + "123");
 console.log("Hello ");
 console.log("Hello ");
->>>>>>> f9686d88ef3034e05152b19d8c9fe6b1cf510c67
